c8db24eec31555839dad89a37fccfec18a09ce0e
Pr-1529314 <pau.ribae@e-campus.uab.cat>
2020-11-14 17:57:37 +0100

    inicialización arreglada y acabada arbol_rec

--- arbre.py
+++ arbre.py
@@ -34,6 +34,9 @@ def load_dataset(path):
     return dataset
 
 def treatdata(dataset):
+
+    n = pd.factorize(dataset[dataset.keys()[-1]].values)
+    dataset[dataset.keys()[-1]] = n[0]
     dataset = dataset.apply(pd.to_numeric, errors='coerce')
 
     dataset1 = dataset.dropna()
@@ -45,62 +48,70 @@ def entropia(ds, atributo=None):
     # ID3
     eps = np.finfo(float).eps
     ent = 0
-    val = ds.play.unique()
+    target = list(ds.keys())[-1]
+    val = ds[target].unique()
 
     if atributo is None:
         for v in val:
-            aux = ds.play.value_counts()[v]/len(ds.play)
+            aux = ds[target].value_counts()[v]/len(ds[target])
             ent += -aux*np.log2(aux)
     else:
         vs = ds[atributo].unique()
         for v in vs:
             ent_v = 0
             for t in val:
-                n = len(ds[atributo][ds[atributo] == v][ds.play == t])
+                n = len(ds[atributo][ds[atributo] == v][ds[target] == t])
                 d = len(ds[atributo][ds[atributo] == v])
                 f = n/(d+eps)
                 ent_v += -f*np.log2(f+eps)
             f2 = d/len(ds)
             ent += -f2*ent_v
-    return ent
+    return abs(ent)
 
 
 def mejor_attr(ds, criterio):
-    llaves = list(ds.keys())[:-1]
-    # Seleccionem el millor atribut segons el guany d'informació o el ratio de guany
-    if criterio == 'ID3':
-        gains = [gains_bruh(ds, atr) for atr in llaves]
-        return llaves[np.argmax(gains)]
+    # Selección del mejor atributo segun el criterio de decisión
+    if criterio == 0:
+        gains = [gains_bruh(ds, atr) for atr in ds.keys()[:-1]]
+        return ds.keys()[:-1][np.argmax(gains)]
     else:
         return
 
 
 def gains_bruh(s, a):
-    return entropia(s)-entropia(s, a);
+    return entropia(s)-entropia(s, a)
 
 
-def arbol_rec(ds,criterio, arbol=None):
+def arbol_rec(ds, criterio, target, arbol=None):
     # Selección del mejor atributo
-    atr = mejor_attr(ds)
+    atr = mejor_attr(ds, criterio)
     # Obtenemos los diferentes valores que puede tomar el atributo
     pos_vals = np.unique(ds[atr])
     if arbol is None:
         arbol = {}
-        arbol[atr]
+        arbol[atr] = {}
     # Construcción del arbol
     for val in pos_vals:
-        new_ds = ds[ds[atr] == val].reset_index(drop=True)
-        # CHRISTIAN SIGUE POR AQUÍ- okiiiiiiiiiiiiiiiiii
-        v, c = np.unique(new_ds['Eat'], return_counts=True)#<-------------------
-        if len(c) == 1:
-            arbol[atr][val] = v[0]
+        # new_ds = ds[ds[atr] == val].reset_index(drop=True)
+        new_ds = ds.drop(columns=[atr])
+        # obtención de los valores únicos de ese atributo y para cada valor único
+        valores, counts = np.unique(new_ds[target], return_counts=True)
+        # Si hemos encontrado un solo valor para el atributo
+        if len(counts) == 1:
+            arbol[atr][val] = valores[0]
+        # Si no llamamos recursivamente a la función
+        else:
+            arbol[atr][val] = arbol_rec(new_ds, criterio, target)
+    return arbol
 
 
 def main():
     dataset = load_dataset('data/ad.data')
     dataset = treatdata(dataset)
-    print(11111)
-
+    criterio = 0 # ID3
+    target = dataset.keys()[-1]
+    arbol = arbol_rec(dataset, criterio, target)
+    print(arbol)
 
 if __name__ == '__main__':
     main()
