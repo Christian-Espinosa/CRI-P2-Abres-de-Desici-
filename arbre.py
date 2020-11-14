fc9d20df88b254d027e2a16ddf48cb4e0a0469a4
Christian <chr.espinosa@gmail.com>
2020-11-14 12:00:31 +0100

    Implementar función de entropia

--- arbre.py
+++ arbre.py
@@ -8,7 +8,6 @@ import seaborn as sns
 # paquet per a manipulacio de dades
 import pandas as pd
 
-
 # funcions i paquets per a carregar datasets especifics (exemples simples)
 from sklearn import datasets
 
@@ -31,13 +30,41 @@ pd.set_option('display.max_columns', 50)
 
 # Funcio per a llegir dades en format csv
 def load_dataset(path):
-    dataset = pd.read_csv(path, header=0, delimiter=',')
+    dataset = pd.read_csv(path, header=None, delimiter=',')
     return dataset
 
-# Carreguem dataset d'exemple
-dataset = load_dataset('/data/ad.data')
+def treatdata(dataset):
+    dataset = dataset.apply(pd.to_numeric, errors='coerce')
 
-dataset = dataset.apply (pd.to_numeric, errors='coerce')
+    dataset1 = dataset.dropna()
+    dataset1 = dataset1.reset_index(drop=True)
+    return dataset1
 
-dataset1 = dataset.dropna()
-dataset1 = dataset1.reset_index(drop=True)
+def entropia(ds, atributo=None):
+    # ID3
+    eps = np.finfo(float).eps
+    ent = 0
+    val = ds.play.unique()
+
+    if atributo is None:
+        for v in val:
+            aux = ds.play.value_counts()[v]/len(ds.play)
+            ent += -aux*np.log2(aux)
+    else:
+        vars = ds[atributo].unique()
+        for v in vars:
+            ent_v = 0
+            for t in val:
+                n = len(ds[atributo][ds[atributo]==v][ds.play==t])
+                d = len(ds[atributo][ds[atributo] == v])
+                f = n/(d+eps)
+    return ent
+
+
+def main():
+    dataset = load_dataset('/data/ad.data')
+    dataset = treatdata(dataset)
+
+
+if __name__ == '__main__':
+    main()
