0598580f505fbb7582e01f3cbf3a9f00f61b7312
Pr-1529314 <pau.ribae@e-campus.uab.cat>
2020-11-15 18:46:25 +0100

    arbol_rec en proceso, falta cross_val, leave_one_out y C4.5

--- arbre.py
+++ arbre.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import time
 # funcions i paquets per a visualitzacio
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -41,32 +41,68 @@ def treatdata(dataset):
 
     dataset1 = dataset.dropna()
     dataset1 = dataset1.reset_index(drop=True)
+    # eliminar columnas con valores no binarios(CAMBIAR POR FACTORIZE)
+    # dataset1 = dataset1.drop(columns=[0, 1, 2])
+    p = pd.factorize(dataset[dataset.keys()[0]])
+    dataset[dataset.keys()[0]] = p[0]
+    p = pd.factorize(dataset[dataset.keys()[1]])
+    dataset[dataset.keys()[1]] = p[0]
+    p = pd.factorize(dataset[dataset.keys()[2]])
+    dataset[dataset.keys()[2]] = p[0]
     return dataset1
 
+"""
 
 def entropia(ds, atributo=None):
     # ID3
     eps = np.finfo(float).eps
     ent = 0
     target = list(ds.keys())[-1]
-    val = ds[target].unique()
+    t_v = ds[target].unique()
 
     if atributo is None:
-        for v in val:
+        for v in t_v:
             aux = ds[target].value_counts()[v]/len(ds[target])
             ent += -aux*np.log2(aux)
     else:
         vs = ds[atributo].unique()
         for v in vs:
             ent_v = 0
-            for t in val:
+            for t in t_v:
                 n = len(ds[atributo][ds[atributo] == v][ds[target] == t])
                 d = len(ds[atributo][ds[atributo] == v])
                 f = n/(d+eps)
                 ent_v += -f*np.log2(f+eps)
             f2 = d/len(ds)
-            ent += -f2*ent_v
+            ent += f2*ent_v
     return abs(ent)
+"""
+
+
+def simple_entr(ds, atr=None):
+    target = ds.keys()[-1]
+    t_v = ds[target].unique()
+    ent = 0
+    if atr is None:
+        for v in t_v:
+            aux = ds[target].value_counts()[v]/len(ds[target])
+            ent += -aux*np.log2(aux)
+    else:
+        pos_v = ds[atr].unique()
+        for val in pos_v:
+            # entropia parcial
+            part_ent = 0
+            # valores del atributo iguales a val
+            atr_val = np.argwhere([ds[atr] == val])[:, 1]
+            # numero de valores del atributo iguales a val
+            atr_prop = len(atr_val)
+            for f_val in t_v:
+                s_vals = np.take(ds[target].values, atr_val)
+                s_prop = len(s_vals[s_vals == f_val])
+                p = (s_prop/atr_prop) if s_prop > 0 else 0
+                part_ent += -p*np.log2(p) if p > 0 else 0
+            ent += part_ent*(atr_prop/len(ds[atr]))
+    return ent
 
 
 def mejor_attr(ds, criterio):
@@ -79,22 +115,23 @@ def mejor_attr(ds, criterio):
 
 
 def gains_bruh(s, a):
-    return entropia(s)-entropia(s, a)
+    return simple_entr(s)-simple_entr(s, a)
 
 
 def arbol_rec(ds, criterio, target, arbol=None):
+    arbol = {}
     # Selección del mejor atributo
     atr = mejor_attr(ds, criterio)
     # Obtenemos los diferentes valores que puede tomar el atributo
     pos_vals = np.unique(ds[atr])
-    if arbol is None:
-        arbol = {}
-        arbol[atr] = {}
+    # creamos nodo atributo
+    arbol[atr] = {}
     # Construcción del arbol
     for val in pos_vals:
+        indices = np.hstack(np.argwhere(ds[atr].values == val))
+        new_ds = ds.drop(index=indices).reset_index(drop=True)
         # new_ds = ds[ds[atr] == val].reset_index(drop=True)
-        new_ds = ds.drop(columns=[atr])
-        # obtención de los valores únicos de ese atributo y para cada valor único
+        new_ds = new_ds.drop(columns=[atr])
         valores, counts = np.unique(new_ds[target], return_counts=True)
         # Si hemos encontrado un solo valor para el atributo
         if len(counts) == 1:
@@ -105,12 +142,29 @@ def arbol_rec(ds, criterio, target, arbol=None):
     return arbol
 
 
+def predict(x_test, arbol):
+    if arbol.dtype is not dict:
+        return arbol
+    else:
+        return
+
+
+
 def main():
     dataset = load_dataset('data/ad.data')
     dataset = treatdata(dataset)
-    criterio = 0 # ID3
+    # particionamos el dataset con un 80% de training y el resto test(20%)
+    train_set = dataset.drop(dataset.index[1887:])
+    test_set = dataset.drop(dataset.index[:1887])
+    # ds = dataset.drop(dataset.index[400:])
+    criterio = 0# ID3
     target = dataset.keys()[-1]
-    arbol = arbol_rec(dataset, criterio, target)
+    beg = time.time()
+    # entrenamos a nuestro clasificador( creamos el arbol)
+    arbol = arbol_rec(train_set, criterio, target)
+    end = time.time()
+    total = (end-beg)
+    print(total)
     print(arbol)
 
 if __name__ == '__main__':
